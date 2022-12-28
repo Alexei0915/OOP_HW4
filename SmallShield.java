63bbe7db13379f23fc8290d5f8934ed3d3286d72
Alexei <vertuginalexei7@gmail.com>
2022-12-28 13:45:05 +0300

    hw4

---
+++ SmallShield.java
@@ -0,0 +1,11 @@
+public class SmallShield extends Shield {
+    @Override
+    public int damageAbsorption() {
+        return 5;
+    }
+
+    @Override
+    public String toString() {
+        return "Маленький щит: " + super.toString();
+    }
+}
